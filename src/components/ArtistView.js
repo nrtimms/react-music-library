8874aff737c404c060fe3223cc6814e75aa16521
nrtimms <nataliertimms18@gmail.com>
2023-07-12 13:59:32 -0700

    router branch

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,15 +1,40 @@
-import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useParams, Link } from 'react-router-dom'
+import NavBar from './NavBar'
 
 function ArtistView() {
-    const { id } = useParams()
-    const [ artistData, setArtistData ] = useState([])
+   const { id } = useParams()
+   const [artistData, setArtistData] = useState([])
 
-    return (
-        <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
-        </div>
-    )
+   useEffect(() => {
+      const API_URL = `http://localhost:4000/album/${id}`
+      const fetchData = async () => {
+         const response = await fetch(API_URL)
+         const resData = await response.json()
+         setArtistData(resData.results)
+      }
+      fetchData()
+   }, [id])
+
+   const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+   const renderAlbums = justAlbums.map((album, i) => {
+      return (
+         <div key= {i} >
+            <Link to ={`/album/${album.collectionId}`}>
+               <p>{album.collectionName}</p>
+            </Link>
+          </div >
+      )
+})
+
+return (
+   <div>
+      <NavBar />
+      <h2>The id passed was: {id}</h2>
+      <p>Artist Data Goes Here!</p>
+      {renderAlbums}
+   </div>
+)
 }
 
 export default ArtistView
